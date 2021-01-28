90f1f93af6a11e9f4d7e92beafdfb743471e10dd
reokoda <atelier12.f1@gmail.com>
2021-01-28 20:58:29 +0900

    SQLにコメント登録できるように変更

--- post_comment_exec.php
+++ post_comment_exec.php
@@ -9,10 +9,48 @@
 
         <?php
 
-        $comment = $_POST["comment"];
-        echo "質問・コメント";
-        echo "<br>\n";
-        echo "${comment}";
+        //接続用パラメータの設定
+        $host = 'localhost'; //データベースが動作するホスト
+        $user = 'www'; //DBユーザ名（各自が設定）
+        $pass = 're12wcd'; //DBパスワード（各自が設定）
+        $dbname = 'comment_db';//データベース名（各自が設定）
+
+        // mysqliクラスのオブジェクトを作成
+        $mysqli = new mysqli($host, $user, $pass, $dbname);
+        if($mysqli->connect_error){
+            echo $mysqli->connect_error;
+            exit();
+        }
+        else{
+            $mysqli->set_charset("utf8");
+        }
+
+        if(!empty($_POST["comment"])){
+            $comment = $_POST["comment"];
+            echo "入力した質問・コメント";
+            echo "<br>";
+            echo "${comment}";
+            echo "<hr>";
+
+            $sql = "insert into comment (mainText) values ('$comment')";
+            $result = $mysqli -> query($sql);
+            if($result){
+                echo "データの登録に成功しました";
+            }
+            else{
+                echo "データの登録に失敗しました";
+                echo "SQL文: $sql";
+                echo "エラー番号: $mysqli->errno";
+                echo "エラ〜メッセージ: $mysqli->error";
+                exit();
+            }
+
+            $mysqli->close();
+        }
+        else{
+            echo "テキストが入力されていません";
+        }
+
         ?>
 
     </body>
